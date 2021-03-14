37d7b5ccc0a22a465091d272eaca3278cf53361e
Mohamed A. Bahnsawy <vipme1@outlook.com>
2021-03-14 22:21:09 +0200

    Updating Sign Up Form

--- signup.php
+++ signup.php
@@ -3,12 +3,14 @@
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $email = $password = $confirm_password = "";
+$username_err = $email_err = $password_err = $confirm_password_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-
+    // Grap Name and Email
+    $name = $_POST['name'];
+    $email = $_POST['email'];
     // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
@@ -46,7 +48,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";
     } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+        $password_err = "Password must have at least 6 characters.";
     } else{
         $password = trim($_POST["password"]);
     }
@@ -65,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password, name, user_email) VALUES (?, ?, '$name', '$email')";
 
         if($stmt = mysqli_prepare($conn, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -123,11 +125,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
     </div>
     <img class="center-image" src="img/1.jpg" alt="">
-
     <div class="container">
         <div class="login-fullpage">
             <div class="row">
-                <div class="login-logo"><img class="center-image" src="img/10.jpg" alt=""></div>
+                <div class="login-logo"><img class="center-image" src="img/11.jpg" alt=""></div>
                 <div class="col-xs-12 col-sm-7">
                     <div class="f-login-content">
                         <div class="f-login-header">
@@ -135,10 +136,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <div class="f-login-desc color-grey">Please Sign Up</div>
                         </div>
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                            <div class="form-group">
+                                <label>Full Name</label>
+                                <input type="text" name="name" class="form-control">
+<!--                                <span class="help-block">--><?php //echo $username_err; ?><!--</span>-->
+                            </div>
                             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                                 <label>Username</label>
                                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
                                 <span class="help-block"><?php echo $username_err; ?></span>
+                            </div>
+                            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                                <label>Email</label>
+                                <input type="email" name="email" class="form-control">
+<!--                                <span class="help-block">--><?php //echo $email_err; ?><!--</span>-->
                             </div>
                             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                                 <label>Password</label>
