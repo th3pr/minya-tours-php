e57da0b292e01f156e5445769118939358f16a9f
Mohamed A. Bahnsawy <vipme1@outlook.com>
2021-03-21 02:33:28 +0200

    Complete Signup and login system

--- login.php
+++ login.php
@@ -1,226 +1,171 @@
 <?php
-ob_start();
+// Initialize the session
 session_start();
-$pageTitle = 'Login';
-if (isset($_SESSION['user'])) {     //  if user register سابقا
-    header('Location: Index.php');   // Redirct To Dashboard
+
+// Check if the user is already logged in, if yes then redirect him to user page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: user.php");
+    exit;
 }
 
-include 'init.php';
+// Include config file
+require_once "config.php";
 
-// Check If User Coming From HTTP Requset Method
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (isset($_POST['login'])) {
-
-        $user   = $_POST['username'];
-        $pass   = $_POST['password'];
-        $hashedpass = sha1($pass);   // To Encrypt The Password
-
-        // Check If User Exists In Database
-
-        $stmt = $con->prepare("SELECT
-                                        UserID, UserName, `Password` 
-                                    FROM 
-                                        users 
-                                    WHERE 
-                                        UserName=?
-                                    AND 
-                                        `Password`=?");
-
-
-        $stmt->execute(array($user, $hashedpass));
-
-        $get = $stmt->fetch();
-
-        $count = $stmt->rowCount();
-
-        // IF Count > 0 This Mean The Database Conatin Record About This Username
-
-        if ( $count > 0) {
-
-            $_SESSION['user'] = $user;   // Register Session Name
-
-            $_SESSION['uid'] = $get['UserID'];
-
-            header('Location: Index.php');   // Redirct To Dashboard
-            exit();
-        }
+    // Check if username is empty
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter username.";
     } else {
-
-        $username   = $_POST['username'];
-        $password   = $_POST['password'];
-        $password2  = $_POST['password2'];
-        $email      = $_POST['Email'];
-
-        $formErrors = array();
-
-        if(isset($username)) {
-
-            $filtredUser = filter_var($username, FILTER_SANITIZE_STRING);
-
-            if(strlen($filtredUser) < 4) {
-
-                $formErrors[] = 'Username Must Be Larger Than 4 Characters';
-            }
-        }
-
-        if(isset($password) && isset($password2)) {
-
-            if(empty($password)) {
-
-                $formErrors[] = 'Password Can\'t be Empty';
-
-            }
-
-            if(sha1($password) !== sha1($password2)) {
-
-                $formErrors[] = 'Sorry The Password Is Not Match';
-
-            }
-        }
-
-        if(isset($email)) {
-
-            $filtredEmail = filter_var($email , FILTER_SANITIZE_EMAIL);
-
-            if ( filter_var($filtredEmail,FILTER_VALIDATE_EMAIL) !=true ) {
-
-                $formErrors[] = 'This Email Is Not Valid';
-
-            }
-        }
-
-        //Check If Ther's No Proccesd The user Add لو كل الحاجات اللي فوق عدي منها
-
-        if(empty($formError)) {
-
-            // Check If The User  Exists In Data Base
-
-            $check = CheckItem("UserName","users",$username);
-
-            if ($check == 1) {
-
-                $formErrors[] = 'Sorry This User Exist In Database';
-
-            } else {
-
-                // Make Insert  For This Value In Datbase
-
-                $stmt = $con->prepare("INSERT INTO 
-                                            users(UserName, `Password`, Email, RegStatus, Date )
-                                        VALUES(:zuser, :zpass, :zmail,0, now())");
-                $stmt->execute(array(
-
-                    'zuser'   => $username,
-                    'zpass'   => sha1($password),
-                    'zmail'   => $email,
-                ));
-
-                // Echo Success Message
-
-                $succesMesg = 'Congrats You Are Registerd Now';
-
-            }
-        }
-
-
+        $username = trim($_POST["username"]);
     }
-}
 
-?>
-<div class="container login-page">
-    <h1 class="text-center">
-        <span class="selected" data-class="login">Login</span> |
-        <span data-class="signup">Signup</span>
-    </h1>
-    <!-- Start The Login Form -->
-    <form class="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <div class="input-container">
-            <input
-                class="form-control"
-                type="text"
-                name="username"
-                autocomplete="off"
-                placeholder="Type Your Username"
-                required />
-        </div>
-        <div class="input-container">
-            <input
-                class="form-control"
-                type="password"
-                name="password"
-                autocomplete="new password"
-                placeholder="Type Your Password"
-                required />
-        </div>
-        <input class="btn btn-primary btn-block" name="login" type="submit" value="Login">
-    </form>
-    <!-- End The Login Form -->
+    // Check if password is empty
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter your password.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
 
-    <!-- Start The Signup Form -->
-    <form class="signup" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <div class="input-container">
-            <input
-                pattern = ".{4,}"
-                title = "Username Must Be 4 chars"
-                class="form-control"
-                type="text"
-                name="username"
-                autocomplete="off"
-                placeholder="Type Your Username"
-                required />
-        </div>
-        <div class="input-container">
-            <input
-                minlength = "4"
-                class="form-control"
-                type="password"
-                name="password"
-                autocomplete="new password"
-                placeholder="Type Your Complex Password"
-                required />
-        </div>
-        <div class="input-container">
-            <input
-                minlength = "4"
-                class="form-control"
-                type="password"
-                name="password2"
-                autocomplete="new password"
-                placeholder="Confirm the password again"
-                required />
-        </div>
-        <div class="input-container">
-            <input
-                class="form-control"
-                type="email"
-                name="Email"
-                placeholder="Type The Email" />
-        </div>
-        <input class="btn btn-success btn-block" name="signup" type="submit" value="Login">
-    </form>
-    <!-- End The Signup Form -->
-    <div class="the-errors text-center">
-        <?php
-        if(! empty($formErrors)) {
+    // Validate credentials
+    if (empty($username_err) && empty($password_err)) {
+        // Prepare a select statement
+        $sql = "SELECT user_id, user_name, user_password FROM users WHERE user_name = ?";
 
-            foreach($formErrors as $error) {
+        if ($stmt = $conn->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-                echo '<div class="msg error">' . $error . '</div>';
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
+                            // Password is correct, so start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["user_id"] = $id;
+                            $_SESSION["user_name"] = $username;
+
+                            // Redirect user to user page
+                            header("location: user.php");
+                        } else {
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else {
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
             }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
+    }
 
-        if (isset($succesMesg)) {
-
-            echo '<div class="msg success">' . $succesMesg . '</div>';
-
-        }
-        ?>
-    </div>
-</div>
-<?php
-include $tep . 'footer.php';
-ob_end_flush();
+    // Close connection
+    mysqli_close($conn);
+}
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta name="format-detection" content="telephone=no" />
+
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/jquery-ui.structure.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <title>Minya Tours | Sign Up</title>
+</head>
+
+<body data-color="mainColor">
+    <!-- LOADER -->
+    <div class="loading dr-blue-2">
+        <div class="loading-center">
+            <div class="loading-center-absolute">
+                <div class="object object_four"></div>
+                <div class="object object_three"></div>
+                <div class="object object_two"></div>
+                <div class="object object_one"></div>
+            </div>
+        </div>
+    </div>
+    <img class="center-image" src="img/1.jpg" alt="">
+    <div class="container">
+        <div class="login-fullpage">
+            <div class="row">
+                <div class="login-logo"><img class="center-image" src="img/11.jpg" alt=""></div>
+                <div class="col-xs-12 col-sm-7">
+                    <div class="f-login-content">
+                        <div class="f-login-header">
+                            <div class="f-login-title color-dr-blue-2">Login!</div>
+                            <div class="f-login-desc color-grey">Please Login</div>
+                        </div>
+                        <?php
+                        if (!empty($login_err)) {
+                            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+                        }
+                        ?>
+
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Login">
+                            </div>
+                            <p>Don't have an account? <a href="signup.php">Sign up now</a>.</p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="full-copy">&copy; 2021 All rights reserved. ITI</div>
+    </div>
+
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/idangerous.swiper.min.js"></script>
+    <script src="js/jquery.viewportchecker.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.mousewheel.min.js"></script>
+    <script src="js/jquery.circliful.min.js"></script>
+    <script src="js/all.js"></script>
+</body>
+
+</html>
